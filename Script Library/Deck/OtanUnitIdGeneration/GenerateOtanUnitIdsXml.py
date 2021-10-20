2ad7228a42759784839a774fec4f1760f239b21b
Kenny Kim <kennykim11@gmail.com>
2021-10-20 00:27:46 -0400

    Fixed units to Blufor

--- Script Library/Deck/OtanUnitIdGeneration/GenerateOtanUnitIdsXml.py
+++ Script Library/Deck/OtanUnitIdGeneration/GenerateOtanUnitIdsXml.py
@@ -6,8 +6,10 @@ import pandas as pd
 
 total_string = ''
 df = pd.read_csv('TUniteDescriptor.csv')
+last_idx=0
 for idx, unit in enumerate(df['ClassNameForDebug']):
-    if unit.startswith('Company_') or unit.startswith('Building'):
+    if unit.startswith('Building_'):
+        last_idx+=1
         total_string += f"""\
             <change property="OtanUnitIds" operation="append" type="map">
                 <map>
@@ -20,8 +22,22 @@ for idx, unit in enumerate(df['ClassNameForDebug']):
                         </reference>
                     </value>
                 </map>
-            </change>
-"""
+            </change>\n"""
+df = pd.read_csv('TUniteAuSolDescriptor.csv')
+for idx, unit in enumerate(df['ClassNameForDebug'], last_idx):
+    total_string += f"""\
+            <change property="OtanUnitIds" operation="append" type="map">
+                <map>
+                    <key type="UInt32">{idx+1}</key>
+                    <value type="ObjectReference">
+                        <reference table="TUniteAuSolDescriptor">
+                            <matchconditions>
+                                <matchcondition type="property" property="ClassNameForDebug">{unit}</matchcondition>
+                            </matchconditions>
+                        </reference>
+                    </value>
+                </map>
+            </change>\n"""
 
 with open('OtanUnitIdChanges.txt', 'w+') as out:
     out.write(total_string)
